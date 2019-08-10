d693b04b8be671bc7248982dd942796113349584
spbryan <seanbryan@comcast.com>
2019-08-10 16:12:55 -0400

    start from scratch

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
-/********************************
-  * Server.js for Bookj Search
-  * 
-  * @author Sean Bryan
-  * 
-  * 2019-08-10
-  ********************************/
-
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-var mongoose = require("mongoose");
-const routes = require("./routes");
 
-//Models
-var db = require("./models");
-
-//Middleware
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/libraryDb";
-mongoose.connect(MONGODB_URI);
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
-app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
